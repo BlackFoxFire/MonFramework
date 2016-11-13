041f1de2cf543e23e234c5e7f2f8dfe3b3dce429
BlackFoxFire <blackfoxfire31@gmail.com>
2016-11-13 15:34:25 +0100

    Modification de la classe Session

--- vendor/MonFramework/Session.php
+++ vendor/MonFramework/Session.php
@@ -1,48 +1,56 @@
 <?php
 
-	/*
-	* Session.php
-	* @Auteur : Christophe Dufour
-	* 
-	* Classe modélisant une session.
-	*
-	*/
+/*
+*
+* Session.php
+* @Auteur : Christophe Dufour
+*
+* Classe modélisant une session.
+* Permet des actions courantes sur les sessions et leurs variables.
+*
+*/
+
+// Espace de nom.
+namespace MonFramework;
+
+/* Définition de la classe. */
+class Session {
 	
-	// Définition de l'espace de nom
-	namespace MonFramework;
-	
-	/* Définition de la classe */
-	class Session {
-	
-		// Démarre ou restaure un session
-		public function __construct() {
-			session_start();
-		}
-		
-		// Détruit la session actuelle
-		public function detruire() {
-			session_destroy();
-		}
-		
-		// Ajoute un variable de session
-		public function set($nom, $valeur) {
-			$_SESSION[$nom] = $valeur;
-		}
-		
-		// Renvoie true si une variable de session existe
-		public function existe($nom) {
-			return(!empty($_SESSION[$nom]));
-		}
-		
-		// Renvoie la valeur d'une variable de session
-		public function get($nom) {
-			if($this->existe($nom)) {
-				return($_SESSION[$nom]);
-			}
-			else {
-				throw new \Exception("Attribut '$nom' absent de la session.");
-			}
-		}
-	
+	// Démarre ou restaure un session.
+	public function __construct() {
+		session_start();
 	}
-	/* Fin de la définition de la classe. */
+	
+	// Détruit la session actuelle.
+	public function destroy() {
+		session_destroy();
+	}
+	
+	// Teste si une variable de session existe.
+	// Renvoie true s'il existe, sinon false.
+	public function _isset($key) {
+		return isset($_SESSION[$key]);
+	}
+	
+	// Teste si une variable de session est vide.
+	// Renvoie true s'il est vide, sinon false.
+	public function _empty($key) {
+		return empty($_SESSION[$key]);
+	}
+	
+	// Ajoute ou modifie une variable de session.
+	public function set($key, $value) {
+		$_SESSION[$key] = $value;
+	}
+	
+	// Renvoie la valeur d'une variable de session.
+	// Si celui ci n'existe pas, c'est returnValue qui est renvoyé.
+	public function get($key, $returnValue = false) {
+		if(self::_isset($key))
+			return $_SESSION[$key];
+		
+		return $returnValue;
+	}
+	
+}
+/* Fin de la classe. */
